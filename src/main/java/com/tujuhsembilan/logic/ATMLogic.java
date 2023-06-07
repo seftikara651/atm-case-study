6effe46b7826962bcb7b0cb18d5a7049fca93cd9
aziztaufiqurrahman <taufiqqurrahman2899@gmail.com>
2023-06-07 10:14:36 +0700

    melengkapi ATM Logic

--- src/main/java/com/tujuhsembilan/logic/ATMLogic.java
+++ src/main/java/com/tujuhsembilan/logic/ATMLogic.java
@@ -4,12 +4,55 @@ package com.tujuhsembilan.logic;
 public class ATMLogic {
 
   public static void login() {
+    System.out.println("Selamat datang di ATM");
+    System.out.println("Silakan masukkan username dan password Anda.");
+
+    System.out.print("Username: ");
+    String inputUsername = scanner.nextLine();
+
+    System.out.print("Password: ");
+    String inputPassword = scanner.nextLine();
+
+    if (inputUsername.equals(username) && inputPassword.equals(password)) {
+      System.out.println("Login berhasil. Selamat datang, " + username + "!");
+    } else {
+      System.out.println("Login gagal. Silakan coba lagi.");
+    }
   }
 
   public static void accountBalanceInformation() {
+    System.out.println("Informasi Saldo Rekening");
+    System.out.print("Masukkan nomor rekening: ");
+    String accountNumber = scanner.nextLine();
+
+    if (accountBalances.containsKey(accountNumber)) {
+      double saldo = accountBalances.get(accountNumber);
+      System.out.println("Saldo rekening " + accountNumber + ": " + saldo);
+    } else {
+      System.out.println("Nomor rekening tidak valid.");
+    }
   }
 
   public static void moneyWithdrawal() {
+    System.out.println("Penarikan Uang");
+    System.out.print("Masukkan nomor rekening: ");
+    String accountNumber = scanner.nextLine();
+
+    if (accountBalances.containsKey(accountNumber)) {
+      double saldo = accountBalances.get(accountNumber);
+      System.out.print("Masukkan jumlah uang yang akan ditarik: ");
+      double withdrawalAmount = scanner.nextDouble();
+
+      if (withdrawalAmount <= saldo) {
+        double newSaldo = saldo - withdrawalAmount;
+        accountBalances.put(accountNumber, newSaldo);
+        System.out.println("Penarikan berhasil. Saldo terkini: " + newSaldo);
+      } else {
+        System.out.println("Saldo tidak mencukupi.");
+      }
+    } else {
+      System.out.println("Nomor rekening tidak valid.");
+    }
   }
 
   public static void phoneCreditsTopUp() {
